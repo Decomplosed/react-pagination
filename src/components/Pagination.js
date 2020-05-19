4b02cf1142ded0df49a378a702b1aa00c1458272
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-19 12:17:08 +0200

    create number of pages

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -3,6 +3,10 @@ import React from 'react'
 const Pagination = ({ postsPerPage, totalPosts }) => {
   const pageNumbers = []
 
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pageNumbers.push(i)
+  }
+
   return <div></div>
 }
 
