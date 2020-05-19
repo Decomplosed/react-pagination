d4cc31d273cb285a2792ea34b99c353ae0471bdd
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-19 10:24:59 +0200

    loop through posts and create list element for each post inside posts array

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -5,7 +5,15 @@ const Posts = ({ posts, loading }) => {
     return <h2>Loading...</h2>
   }
 
-  return <div></div>
+  return (
+    <ul className='list-group mb-4'>
+      {posts.map((post) => (
+        <li key={post.id} className='list-group-item'>
+          {post.title}
+        </li>
+      ))}
+    </ul>
+  )
 }
 
 export default Posts
