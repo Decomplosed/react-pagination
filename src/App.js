18e05a896f8afada04c1a59750b9083006fcc678
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-19 12:23:23 +0200

    created paginate method which calls setCurrentPage

--- src/App.js
+++ src/App.js
@@ -25,6 +25,8 @@ const App = () => {
   const indexOfFirstPost = indexOfLastPost - postsPerPage
   const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost)
 
+  const paginate = (pageNumber) => setCurrentPage(pageNumber)
+
   return (
     <div className='container mt-5'>
       <h1 className='text-primary mb-3'>My Blog</h1>
