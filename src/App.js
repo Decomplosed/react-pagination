08a44140e3358c7307e5a0fc6e1958e2462fb867
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-19 12:18:44 +0200

    imported Pagination to the App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Posts from './components/Posts'
+import Pagination from './components/Pagination'
 import './App.css'
 
 const App = () => {
