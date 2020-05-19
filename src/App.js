f43f40de8a7f2829233bf474ad8ff33be7168132
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-19 12:21:37 +0200

    pass paginate as a prop

--- src/App.js
+++ src/App.js
@@ -29,7 +29,11 @@ const App = () => {
     <div className='container mt-5'>
       <h1 className='text-primary mb-3'>My Blog</h1>
       <Posts posts={currentPosts} loading={loading} />
-      <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} />
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        paginate={paginate}
+      />
     </div>
   )
 }
