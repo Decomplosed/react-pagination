2d141a0c7fbafd5254f084e411f1564f6fb333e8
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-19 10:25:46 +0200

    setup the classnames for container

--- src/App.js
+++ src/App.js
@@ -20,8 +20,8 @@ const App = () => {
   }, [])
 
   return (
-    <div className='App'>
-      <h1>Clean</h1>
+    <div className='container mt-5'>
+      <h1 className='text-primary mb-3'>My Blog</h1>
     </div>
   )
 }
